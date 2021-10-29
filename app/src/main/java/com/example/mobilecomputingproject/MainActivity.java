736535b2884ba9a0771dbc9f26ada91da614a639
DavidDSS <daviddss1999@gmail.com>
2021-10-29 13:29:09 -0400

    Implemented App Toolbar

--- app/src/main/java/com/example/mobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mobilecomputingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize App Toolbar
+        Toolbar appToolbar = (Toolbar) findViewById(R.id.nutri_toolbar);
+
+        //Set app toolbar and remove default title
+        setSupportActionBar(appToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
