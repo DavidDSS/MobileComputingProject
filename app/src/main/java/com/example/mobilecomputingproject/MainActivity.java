81341b9e12f249b25b81267b60fb97c413394bba
DavidDSS <daviddss1999@gmail.com>
2021-11-23 11:11:28 -0500

    Added list logic to the MainActivity

--- app/src/main/java/com/example/mobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingproject/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -25,6 +26,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,12 +45,39 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         //Initialize Screen Elements
+
+        //Initialize Buttons
         Button nextPageBtn = findViewById(R.id.nextStepBtn);
+        Button addIngredientBtn = findViewById(R.id.addBtn);
+
+        //Initialize User Input Field
         EditText userInputText= findViewById(R.id.ingredientEditText);
+
+        //Initialize Ingredient ListView, List and Empty List Text
         ListView ingredientListView= findViewById(R.id.ingredientList);
+        ArrayList<String> ingredientList= new ArrayList<String>();;
+        TextView emptyListText= findViewById(R.id.emptyListText);
+        ingredientListView.setEmptyView(emptyListText);
+
+        //Create List Adapter and Set it
+        ArrayAdapter<String> ingredientListAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, ingredientList);
+        ingredientListView.setAdapter(ingredientListAdapter);
 
         //Instantiate spoonDataService for API use
         final SpoonDataService spoonDataService= new SpoonDataService(MainActivity.this);
+
+        //Add Ingredient Functionality
+        addIngredientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!userInputText.getText().toString().equals("")) {
+                    ingredientList.add(userInputText.getText().toString());
+                    ingredientListAdapter.notifyDataSetChanged();
+                    userInputText.setText("");
+                }
+            }
+        });
 
         //Next Step Button Functionality
         nextPageBtn.setOnClickListener(new View.OnClickListener() {
@@ -61,11 +90,11 @@ public class MainActivity extends AppCompatActivity {
                     }
                     @Override
                     public void onResponse(List<RecipeModel> recipeList) {
-                        ArrayAdapter arrayAdapter= new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, recipeList);
-                        ingredientListView.setAdapter(arrayAdapter);
+                        //ArrayAdapter arrayAdapter= new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, recipeList);
+                        //ingredientList.setAdapter(arrayAdapter);
 
                         //Got to Filters Page
-                        goToFilters(view);
+                        //goToFilters(view);
                     }
                 });
             }
