d10e40dd55f79fabffd91b2b32999b4478065218
griffin <gallen1999@live.com>
2021-11-30 18:54:55 -0500

    working on filter page

--- app/src/main/java/com/example/mobilecomputingproject/FilterPageActivity.java
+++ app/src/main/java/com/example/mobilecomputingproject/FilterPageActivity.java
@@ -6,15 +6,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class FilterPageActivity extends AppCompatActivity {
 
     RelativeLayout pageLayout;
+    ArrayList<CheckBox> cuisines;
+    ArrayList<RadioButton> diets;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +55,8 @@ public class FilterPageActivity extends AppCompatActivity {
                 "Ovo-Vegetarian", "Vegan", "Pescetarian", "Paleo",
                 "Primal", "Low FODMAP", "Whole30"
         };
-        createCheckboxArray(diets, "DietCheckbox", R.id.dietText, 56000);
+        this.diets = new ArrayList<>();
+        createDietsRadioButtons(diets, "DietCheckbox", R.id.dietText, 56000);
     }
 
 
@@ -59,10 +68,11 @@ public class FilterPageActivity extends AppCompatActivity {
                 "Latin American", "Mediterranean", "Mexican", "Middle Eastern",
                 "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"
         };
-        createCheckboxArray(cuisines, "CuisineCheckbox", R.id.cuisineText, 57000);
+        this.cuisines = new ArrayList<>();
+        createCuisineCheckboxes(cuisines, "CuisineCheckbox", R.id.cuisineText, 57000);
     }
 
-    public void createCheckboxArray(String[] array, String tagName, int placeBelowID, int idStart){
+    public void createCuisineCheckboxes(String[] array, String tagName, int placeBelowID, int idStart){
         for (int i=0; i<array.length; i++) {
             CheckBox temp =  new CheckBox(this);
             temp.setTag(array[i]+tagName);
@@ -82,7 +92,52 @@ public class FilterPageActivity extends AppCompatActivity {
 
             temp.setText(array[i]);
             temp.setLayoutParams(params);
+
+            cuisines.add(temp);
+
             pageLayout.addView(temp, params);
         }
     }
+
+    public void createDietsRadioButtons(String[] array, String tagName, int placeBelowID, int idStart){
+        RadioGroup group = (RadioGroup) findViewById(R.id.radioButtonGroup);
+        for (int i=0; i<array.length; i++) {
+            RadioButton temp =  new RadioButton(this);
+            temp.setTag(array[i]+tagName);
+            temp.setId(idStart+i);
+            temp.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            if (i>0) {
+                if((i%3)>0) params.addRule(RelativeLayout.RIGHT_OF, idStart+i-1);
+                if ((i/3)!=0) params.addRule(RelativeLayout.BELOW, idStart+i-3);
+                else params.addRule(RelativeLayout.BELOW, placeBelowID);
+                System.out.println(findViewById(idStart+i-1).toString());
+            }
+            else params.addRule(RelativeLayout.BELOW, placeBelowID);
+
+            temp.setText(array[i]);
+            temp.setLayoutParams(params);
+
+            diets.add(temp);
+
+            group.addView(temp, params);
+        }
+    }
+
+    public void getRecipes(View view){
+        for (RadioButton diet : diets) {
+            if (diet.isChecked()){
+                // search for the diet in spoonacular
+            }
+        }
+        for (CheckBox cuisine : cuisines) {
+            if (cuisine.isChecked()){
+                // search for the cuisineget in spoonacular
+            }
+        }
+    }
+
 }
